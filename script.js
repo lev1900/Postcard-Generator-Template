f28f14608fc2070bd788bae1cfff409ed37d7d01
Levi <127esacramento@frhsd.com>
2025-11-12 14:06:04 -0500

    rdd

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function ChangeStateList() {
-  var list = document.getElementById("#AK");
+const dropDown = document.getElementById('states');
+const postCard = document.getElementById('postcard');
+const greetText = document.getElementById('greeting');
+const stateHead = document.getElementById('state-heading');
 
-}
-console.log(list);
+dropDown.addEventListener('change', function() {
+    const selectedState = dropDown;
+    if (selectedState) {
+        greetText.textContent = `Greetings from ${selectedState}!`;
+
+    } else {
+        postCard.style.display = 'none';
+    }
+});
