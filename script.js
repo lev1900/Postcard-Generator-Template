6a025b867bf511e7c77d65c2d8b6311c11faad2b
Levi <127esacramento@frhsd.com>
2025-11-13 13:48:18 -0500

    mm ,.,

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const greetText = document.getElementById('greeting');
 const stateHead = document.getElementById('state-heading');
 
 dropDown.addEventListener('change', function() {
-    const selectedState = dropDown;
-    if (selectedState) {
-        greetText.textContent = `Greetings from ${selectedState}!`;
+    const selectedState = dropDown.value;
+    console.log(selectedState);
+    if (selectedState){
+        document.getElementById('greeting').textContent = `Greetings from ${selectedState}!`;
+        document.getElementById('state-heading').textContent = selectedState;
+        postCard.style.backgroundImage = `url('imgs/${selectedState.toLowerCase()}.jpg')`;
 
-    } else {
-        postCard.style.display = 'none';
-    }
-});
+   } 
+}
+);
